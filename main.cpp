022ac49d3691acf76c805c49bc688767e1262b2a
Andrew <andrijpastuh@gmail.com>
2024-02-29 13:59:50 +0200

    Змінили main.ccp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
+#include <vector>
 #include "Mouse.h"
 #include "Keyboard.h"
 #include "Headphones.h"
+#include "MoveExample.h"
 using namespace std;
 int main() {
-    Keyboard warmila("Warmila",2,"plactic");
-    Mouse logitech("Logitech",0.3,"aluminium",true);
-    Headphones hyperx("HyperX",10,"metal",true);
+//    Headphones a("123",123,"dsad",true);
+//    Headphones b(std::move(a));
+//    b.info();
+//     Keyboard warmila("warmola",12,"qweqe");
+//    warmila.get_weight();
+//    const Keyboard b("123",123,"123");
+//    b.get_weight();
+//    Mouse m("123",123,"123123", true);
+//    m.get_amountOfMice();
+//    cin>>m;
+//    cout<<m;
+//    Keyboard a("213123",123,"123123");
+//    Keyboard b(a);
+//    b.info();
+//    a.info();
+//    Mouse a("123",0,"123123");
+//    a+4;
+//    cout<<a;
+//    a+=29.2;
+//    cout<<a;
+//    a-3;
+//    cout<<a;
+//    a-=12.444;
+//    cout<<a;
 
-    warmila.info();
-    logitech.info();
-    hyperx.info();
+
+
     return 0;
 }
