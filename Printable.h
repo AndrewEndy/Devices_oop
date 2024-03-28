d93fec23817aae399c080b7ae49813e9ea3a37b2
Andrew <andrijpastuh@gmail.com>
2024-03-28 13:34:22 +0200

    Change class Printable
    -Delete abstract function print_class_name
    
    -Add friend operator>>
    
    -Add abstract function readData
    
    -Add abstract function writeProduct

--- Printable.h
+++ Printable.h
@@ -11,14 +11,13 @@ using namespace std;
 
 class Printable {
     friend ostream &operator<<(ostream &os, const Printable &obj);
+    friend std::istream &operator>>(istream &is, Printable &obj);
 
 public:
     virtual void print(ostream &os) const = 0;
+    virtual void readData(std::istream &is) = 0;
+    virtual void writeProduct()=0;
     virtual ~Printable() {};
-    virtual void print_class_name()const=0;
 };
-
-
-
 
 #endif //DEVICESOOP_PRINTABLE_H
