52d678864ff660dad5f67f73ea57339fc3541ec7
Andrew <andrijpastuh@gmail.com>
2024-03-28 13:11:32 +0200

    Change class MechanicKeyboard
    -Delete function info
    
    -Delete function doSomething
    
    -Delete virtual function print_class_name
    
    -Change virtual function print
    
    -Change constructor with default parameters
    
    -Add virtual function readData
    
    -Add virtual function writeProduct

--- MechanicKeyboard.h
+++ MechanicKeyboard.h
@@ -4,9 +4,9 @@
 
 #ifndef DEVICESOOP_MECHANICKEYBOARD_H
 #define DEVICESOOP_MECHANICKEYBOARD_H
+
 #include "Keyboard.h"
 #include <iostream>
-
 using namespace std;
 
 class MechanicKeyboard: public Keyboard{
@@ -14,19 +14,16 @@ private:
     string colorOfKeyboard;
 public:
     string set_colorOfKeyboard(string &&colorOfKeyboard);
-    string get_colorOfKeyboard();
-    void info();
-
-    void doSomething()const;
+    string get_colorOfKeyboard()const;
 
     void print(std::ostream &os) const override;
-    void print_class_name()const override;
-
-    MechanicKeyboard(string name="None",float weight=0, string typeOfMaterial="None",string &&colorOfKeyboard="None");
+    void readData(std::istream& is) override;
+    void writeProduct() override;
     MechanicKeyboard(const MechanicKeyboard &other);
+
+    MechanicKeyboard(int product_number=0,string &&name="None", float weight=0, string &&type_of_material="None"
+            ,float price=0,string &&colorOfKeyboard="None");
     ~MechanicKeyboard() override;
 
 };
-
-
 #endif //DEVICESOOP_MECHANICKEYBOARD_H
