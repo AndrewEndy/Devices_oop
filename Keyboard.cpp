0074c69c3942fcf12aa9ef7360e48e24b97d02a3
Andrew <andrijpastuh@gmail.com>
2024-03-28 13:04:04 +0200

    Change class Keyboard
    -Delete function doSomething
    
    -Delete function info
    
    -Delete virtual function print_class_name
    
    -Delete overloaded constructor
    
    -Add constructor with default parameters
    
    -Add fields product_number and price
    
    -Add function set_product_number, get_product_number, set_price, get_price
    
    -Change virtual function print
    
    -Add virtual function readData
    
    -Add virtual function writeProduct

--- Keyboard.cpp
+++ Keyboard.cpp
@@ -5,7 +5,15 @@
 #include "Keyboard.h"
 #include "Printable.h"
 #include <iostream>
+#include <fstream>
+using namespace std;
 
+int Keyboard::set_product_number(int product_number) {
+    return this->product_number=product_number;
+}
+int Keyboard::get_product_number() const {
+    return product_number;
+}
 string Keyboard::set_name(string name)
 {
     return this->name=name;
@@ -13,15 +21,6 @@ string Keyboard::set_name(string name)
 string Keyboard::get_name() const
 {
     return name;
-}
-string Keyboard::set_TypeOfMaterial(string typeOfMaterial)
-{
-
-    return type_of_material=typeOfMaterial;
-}
-string Keyboard::get_TypeOfMaterial() const
-{
-    return type_of_material;
 }
 float Keyboard::set_weight(float weight)
 {
@@ -31,65 +30,71 @@ float Keyboard::get_weight() const
 {
     return weight;
 }
-void Keyboard::info() const
+string Keyboard::set_typeOfMaterial(string type_of_material)
 {
-    cout<<"INFO: "<<endl
-        <<"Назва клавіатури: "<<name
-        <<"\nВага клавіатури: "<<weight<<" кг"
-        <<"\nТип матеріалу клавіатури: "<<type_of_material<<endl;
+
+    return this->type_of_material=type_of_material;
+}
+string Keyboard::get_typeOfMaterial() const
+{
+    return type_of_material;
+}
+float Keyboard::set_price(float price) {
+    return this->price=price;
+}
+float Keyboard::get_price() const {
+    return price;
 }
 
-void Keyboard::doSomething() const
-{
-    cout<<"Keyboard"<<endl;
+void Keyboard::writeProduct() {
+    ofstream file("C:\\Users\\Endi\\CLionProjects\\DevicesOOP\\files\\Products.txt", ios::app);
+    string data=to_string(product_number)+","+name+","+ to_string(weight)+","+type_of_material+","+to_string(price);
+    file<<data;
+    file.close();
+
 }
+
 
 void Keyboard::print(std::ostream &os) const {
-    os<<"INFO: "<<endl
-        <<"Назва клавіатури: "<<name
-        <<"\nВага клавіатури: "<<weight<<" кг"
-        <<"\nТип матеріалу клавіатури: "<<type_of_material<<endl;
+    os<<endl<<"---Товар: Клавіатура---"<<endl
+        <<"Номер товару: "<<product_number<<endl
+        <<"Назва: "<<name<<endl
+        <<"Вага: "<<weight<<endl
+        <<"Матеріал: "<<type_of_material<<endl
+        <<"Ціна: "<<price<<endl;
 }
 
-void Keyboard::print_class_name() const {
-    cout<<"Class name: Keyboard"<<endl;
+void Keyboard::readData(std::istream &is) {
+
+    cout<<"Введіть номер товару: ";
+    is >> product_number;
+
+    cout<<"Введіть назву клавіатури: ";
+    is >> name;
+    cout<<"Введіть вагу клавіатури: ";
+    is >> weight;
+    cout<<"Введіть матеріал клавіатури: ";
+    is >> type_of_material;
+    cout<<"Введіть ціну клавіатури: ";
+    is >> price;
 }
 
-Keyboard::Keyboard()
-{
-    name="None";
-    weight=0;
-    type_of_material="None";
-    cout<<"called Keyboard constructor"<<endl;
-}
-Keyboard::Keyboard(std::string &&name)
-{
-    this->name=name;
-    weight=0;
-    type_of_material="None";
-    cout<<"called Keyboard constructor"<<endl;
-}
-Keyboard::Keyboard(std::string &&name, float weight)
-{
-    this->name=name;
-    this->weight=weight;
-    type_of_material="None";
-    cout<<"called Keyboard constructor"<<endl;
-}
-Keyboard::Keyboard(std::string &&name, float weight, std::string &&type_of_material)
-{
-    this->name=name;
-    this->weight=weight;
-    this->type_of_material=type_of_material;
-    cout<<"called Keyboard constructor"<<endl;
-}
 Keyboard::Keyboard(const Keyboard &other) {
+    product_number=other.product_number;
     name = other.name;
     weight = other.weight;
     type_of_material = other.type_of_material;
+    price = other.price;
     cout<<"called copy constructor"<<endl;
 }
+
+Keyboard::Keyboard(int product_number,string &&name, float weight,string &&type_of_material,float price):
+    product_number{product_number},name{name}, weight{weight}, type_of_material{type_of_material},price{price}
+{
+    //cout<<"called Keyboard constructor"<<endl;
+}
+
 Keyboard::~Keyboard()
 {
-    cout<<"called Keyboard destructor"<<endl;
+    //cout<<"called Keyboard destructor"<<endl;
 }
